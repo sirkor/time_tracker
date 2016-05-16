7d81253dbe74d7df1d7424dd975670a98fd7b04f
sirkor <dodkan@yandex.ru>
2016-05-16 17:20:24 +0300

    +

--- project_time_tracker/time_tracker/tests.py
+++ project_time_tracker/time_tracker/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+import datetime
 
-# Create your tests here.
+
+class ActivitiesTest(TestCase):
+
+    def end_less_then_start(self):
+        start = datetime.datetime(2016, 1, 1)
+        end = datetime.datetime(2015, 1, 1)
+        during = end - start
+        self.assertLess(during, datetime.timedelta(0), msg='True')
