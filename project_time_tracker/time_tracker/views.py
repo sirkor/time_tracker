d46be76601a5145f28f912f56ca5bf9f18847596
sirkor <dodkan@yandex.ru>
2016-05-24 15:22:07 +0300

    Общие исправления: Исправлены переносы строк Весь код по PEP8

--- project_time_tracker/time_tracker/views.py
+++ project_time_tracker/time_tracker/views.py
@@ -6,85 +6,24 @@ from django.db.models import Sum, DurationField
 
 
 def index(request):
+    """Index page"""
     return render_to_response('main.html',
                               context_instance=RequestContext(request))
 
 
 def activities_list(request, username=''):
+    """ list of user activities  """
     args = {'activities': Activities.objects.filter(activities_user=username)}
-    return render_to_response('activities.html',
-                              args,
-                              context_instance=RequestContext(request))
+    return render_to_response('activities.html', args, context_instance=RequestContext(request))
 
 
 def thanks_view(request):
-    return render_to_response('thanks.html',
-                              context_instance=RequestContext(request))
-
-
-def add_template(request, username=''):
-    return render_to_response('add_activity.html',
-                              context_instance=RequestContext(request))
-
-
-def add_activity_view(request, username=''):
-    if request.POST:
-        name = request.POST.get('activities_name', '')
-        my_type = request.POST.get('activities_type', '')
-        start = request.POST.get('activities_start', '')
-        end = request.POST.get('activities_end', '')
-        new_start = datetime.datetime.strptime(start, "%Y-%m-%d %H:%M")
-        new_end = datetime.datetime.strptime(end, "%Y-%m-%d %H:%M")
-        duration = new_end - new_start
-        new_post = Activities(activities_user=username,
-                              activities_name=name,
-                              activities_type=my_type,
-                              activities_start=start,
-                              activities_end=end, activities_duration=duration,
-                              new=request.user,
-                              add_date=datetime.datetime.now())
-        new_post.save()
-        return redirect('/users/thanks')
-    return render_to_response('add_activity.html',
-                              context_instance=RequestContext(request))
-
-
-def statistic_view(request, username=''):
-    now = datetime.datetime.now()
-    statistic = Activities.objects.\
-        filter(activities_user=request.user.username)
-    sum_of_duration = datetime.timedelta(0)
-    for stat in statistic:
-        if stat.activities_start.month == now.month:
-            sum_of_duration += stat.activities_duration
-
-    statistic = Activities.objects.\
-        filter(activities_user=request.user.username, activities_type='Работа')
-    work_duration = datetime.timedelta(0)
-    for stat in statistic:
-        if stat.activities_start.month == now.month:
-            work_duration += stat.activities_duration
-
-    statistic = Activities.objects.\
-        filter(activities_user=request.user.username,
-               activities_type='Остальное')
-    other_duration = datetime.timedelta(0)
-    for stat in statistic:
-        if stat.activities_start.month == now.month:
-            other_duration += stat.activities_duration
-
-    percent_of_work_duration = work_duration / sum_of_duration * 100
-    percent_of_other_duration = other_duration / sum_of_duration * 100
-    args = {'sum_duration': sum_of_duration, 'work_duration': work_duration,
-            'other_duration': other_duration,
-            'percent_of_work_duration': round(percent_of_work_duration, 2),
-            'percent_of_other_duration': round(percent_of_other_duration, 2)}
-
-    return render_to_response('statistic.html', args,
-                              context_instance=RequestContext(request))
+    """ success view for add_activity """
+    return render_to_response('thanks.html', context_instance=RequestContext(request))
 
 
 def statistic(request, username=''):
+    """View for statistic"""
     if request.user.username == username:
         now = datetime.datetime.now()
         now1 = now - datetime.timedelta(30)
@@ -104,10 +43,14 @@ def statistic(request, username=''):
                 'percent_of_work_duration': round(percent_of_work_duration, 2),
                 'percent_of_other_duration': round(percent_of_other_duration, 2)}
         return render_to_response('statistic.html', args, context_instance=RequestContext(request))
+    else:
+        args = {'sorry': 'Эта не ваша страница'}
+        return render_to_response('statistic.html', args, context_instance=RequestContext(request))
     return render_to_response('statistic.html', context_instance=RequestContext(request))
 
 
 def add_activity(request, username=''):
+    """view for form ActivityAddForm"""
     if request.POST:
         form = ActivityAddForm(request.POST)
         if form.is_valid():
@@ -122,4 +65,5 @@ def add_activity(request, username=''):
         form = ActivityAddForm()
         args = {'form': form}
     return render_to_response('add_activity.html', args, context_instance=RequestContext(request))
+
 
